1e05dddc10364adcce41eecf4d3f76cdd0f69d91
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:56:01 -0700

    fix: replace force-unwraps with safe casts in User initializer

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -11,33 +11,31 @@ import Foundation
 class User {
 
     var name: String?
-  var screenname: String?
-  var profileUrl: URL?
-  var tagline: String?
-  var dictionary: NSDictionary?
-  var coverPhotoUrl: URL?
-  var tweetsCount: Int = 0
-  var followerCount: Int = 0
-  var followingCount: Int = 0
+    var screenname: String?
+    var profileUrl: URL?
+    var tagline: String?
+    var dictionary: NSDictionary?
+    var coverPhotoUrl: URL?
+    var tweetsCount: Int = 0
+    var followerCount: Int = 0
+    var followingCount: Int = 0
 
-  init(dictionary: [String: Any]) {
-        name = dictionary["name"] as! String
-      screenname = dictionary["screen_name"] as? String
-      let profileUrlString = dictionary["profile_image_url_https"] as? String
-      if let profileUrlString = profileUrlString{
-        profileUrl = URL(string: profileUrlString)
-      } else {
-        profileUrl = URL(string: "")
-      }
-      let coverPhotoUrlString = dictionary["profile_background_image_url_https"] as? String
-      if let coverPhotoUrlString = coverPhotoUrlString {
-        coverPhotoUrl = URL(string: coverPhotoUrlString)
-      } else {
-        coverPhotoUrl = URL(string: "")
-      }
-      tweetsCount = dictionary["statuses_count"] as! Int
-      tagline = dictionary["description"] as? String
-      followerCount = dictionary["followers_count"] as! Int
-      followingCount = dictionary["friends_count"] as! Int
+    init?(dictionary: [String: Any]) {
+        guard let userName = dictionary["name"] as? String else { return nil }
+        name = userName
+        screenname = dictionary["screen_name"] as? String
+
+        if let profileUrlString = dictionary["profile_image_url_https"] as? String {
+            profileUrl = URL(string: profileUrlString)
+        }
+
+        if let coverPhotoUrlString = dictionary["profile_background_image_url_https"] as? String {
+            coverPhotoUrl = URL(string: coverPhotoUrlString)
+        }
+
+        tweetsCount = dictionary["statuses_count"] as? Int ?? 0
+        tagline = dictionary["description"] as? String
+        followerCount = dictionary["followers_count"] as? Int ?? 0
+        followingCount = dictionary["friends_count"] as? Int ?? 0
     }
 }
