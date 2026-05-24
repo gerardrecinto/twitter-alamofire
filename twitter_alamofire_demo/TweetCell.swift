8a5db13e0ecac3ac9622f0845a642ded78c7f629
Gerard Louis Andres Recinto <grecinto@ucsd.edu>
2026-05-24 15:56:18 -0700

    fix: guard force-unwraps on profileUrl, favoriteCount, and screenname in TweetCell

--- twitter_alamofire_demo/TweetCell.swift
+++ twitter_alamofire_demo/TweetCell.swift
@@ -7,14 +7,12 @@
 //
 
 import UIKit
-import Alamofire
 import AlamofireImage
 
 class TweetCell: UITableViewCell {
 
   @IBOutlet weak var profileButton: UIImageView!
   @IBOutlet weak var tweetTextLabel: UILabel!
-  //@IBOutlet weak var profileButton: UIButton!
   @IBOutlet weak var authorButton: UILabel!
   @IBOutlet weak var screennameLabel: UILabel!
   @IBOutlet weak var dateLabel: UILabel!
@@ -33,124 +31,94 @@ class TweetCell: UITableViewCell {
   var didLike = false
   var timeDiff: Int = 0
   var timeString: String = ""
-  //var user: User!
+
     var tweet: Tweet! {
         didSet {
             tweetTextLabel.text = tweet.text
-          authorButton.text = tweet.name as? String
-          screennameLabel.text = "@" + (tweet.screenname as? String)!
-         // dateLabel.text = String(describing: tweet.timestamp)
+            authorButton.text = tweet.name
+            screennameLabel.text = "@" + (tweet.screenname ?? "")
+            dateLabel.text = timeString
 
-       /*   timeDiff = Int(Date().timeIntervalSince(tweet.createdAtString as! Date))
-          if timeDiff < minute {
-            timeString = String("less than a minute ago")
-          }
-          else if timeDiff < hour {
-            timeDiff = timeDiff/minute
-            timeString = String(timeDiff) + "m"
-          }
-          else if timeDiff < day {
-            timeDiff = timeDiff/hour
-            timeString = String(timeDiff) + "h"
-          }
-          else {
-            timeDiff = timeDiff/day
-            timeString = String(timeDiff) + " days ago"
-          }*/
+            let replyImage = UIImage(named: "reply-icon")
+            replyButton.setImage(replyImage, for: .normal)
 
-          dateLabel.text = timeString
+            if tweet.retweeted == false {
+                let retweetImage = UIImage(named: "retweet-icon")
+                retweetButton.setImage(retweetImage, for: .normal)
+                didRt = false
+            } else {
+                let retweetImage = UIImage(named: "retweet-icon-green")
+                retweetButton.setImage(retweetImage, for: .normal)
+                didRt = true
+            }
 
-          let replyImage = UIImage(named: "reply-icon")
-          replyButton.setImage(replyImage, for: .normal)
+            if tweet.favorited == false {
+                let likeImage = UIImage(named: "favor-icon")
+                likeButton.setImage(likeImage, for: .normal)
+                didLike = false
+            } else {
+                let likeImage = UIImage(named: "favor-icon-red")
+                likeButton.setImage(likeImage, for: .normal)
+                didLike = true
+            }
 
-          if tweet.retweeted == false {
-            let retweetImage = UIImage(named: "retweet-icon")
-            retweetButton.setImage(retweetImage, for: .normal)
-            didRt = false
-          }
-          else {
-            let retweetImage = UIImage(named: "retweet-icon-green")
-            retweetButton.setImage(retweetImage, for: .normal)
-            didRt = true
-          }
+            likeCount = tweet.favoriteCount ?? 0
+            rtCount = tweet.retweetCount
+            setButtonCount()
 
-          if tweet.favorited == false {
-            let likeImage = UIImage(named: "favor-icon")
-            likeButton.setImage(likeImage, for: .normal)
-            didLike = false
-          }
-          else {
-            let likeImage = UIImage(named: "favor-icon-red")
-            likeButton.setImage(likeImage, for: .normal)
-            didLike = true
-          }
-
-          likeCount = tweet.favoriteCount!
-          rtCount = tweet.retweetCount
-          setButtonCount()
-
-        //  profileButton.af_setImage(for: .normal, url: tweet.profileUrl!)
-          profileButton.af_setImage(withURL: tweet.profileUrl!)
-         // myButton.imageView?.contentMode = .scaleAspectFit
-      //    profileButton.imageView?.contentMode = .scaleAspectFill
-        //  profileButton.imageView?.updateConstraintsIfNeeded()
+            if let profileUrl = tweet.profileUrl {
+                profileButton.af_setImage(withURL: profileUrl)
+            }
         }
     }
 
   @IBAction func onLike(_ sender: Any) {
-    if didLike{
+    if didLike {
       likeCount = likeCount - 1
-    }
-    else {
+    } else {
       likeCount = likeCount + 1
     }
     didLike = !didLike
     setButtonCount()
   }
+
   @IBAction func onRetweet(_ sender: Any) {
     if didRt {
       rtCount -= 1
-    }
-    else {
+    } else {
       rtCount += 1
     }
     didRt = !didRt
     setButtonCount()
   }
+
   override func awakeFromNib() {
         super.awakeFromNib()
-        authorButton.preferredMaxLayoutWidth = authorButton.frame.size.width// Initialization code
+        authorButton.preferredMaxLayoutWidth = authorButton.frame.size.width
     }
+
   override func layoutSubviews() {
     super.layoutSubviews()
-    authorButton.preferredMaxLayoutWidth = authorButton.frame.size.width// Initialization code
-
+    authorButton.preferredMaxLayoutWidth = authorButton.frame.size.width
   }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
   func setButtonCount() {
     if likeCount < 1000 {
       likeCountString = String(likeCount)
-    }
-    else {
+    } else {
       likeCountString = String((likeCount/1000)) + "K"
     }
-
     likeButton.setTitle(likeCountString, for: .normal)
 
     if rtCount < 1000 {
       rtCountString = String(rtCount)
-    }
-    else {
+    } else {
       rtCountString = String((rtCount/1000)) + "K"
     }
     retweetButton.setTitle(rtCountString, for: .normal)
   }
-
-
 }
